cdc34347dccc2fabb3c82fb9f364848ada5caf33
KittichaiC <kittichai.c@kbtg.tech>
2021-09-07 16:34:26 +0700

    package users

---
+++ src/test/java/com/example/demoapp/employees/EmployeeControllerServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.demoapp.employees;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerServiceTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @MockBean
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    @DisplayName("Success")
+    public void case01() {
+        // Arrange
+        int id = 1;
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Mock name");
+        when(employeeRepository.findById(1)).thenReturn(Optional.of(employee));
+
+        // Act
+        EmployeeResponse result
+                = restTemplate.getForObject("/employees/" + id, EmployeeResponse.class);
+        // Assert
+        assertEquals(id, result.getId());
+        assertEquals("Mock name", result.getName());
+    }
+
+    @Test
+    @DisplayName("Failure case :: Employee not found id = 100")
+    public void case02() {
+        // Arrange
+        int id = 100;
+        when(employeeRepository.findById(100)).thenReturn(Optional.empty());
+
+        //Act
+        ResponseEntity<ErrorResponse> result
+                = restTemplate.getForEntity("/employees/" + id, ErrorResponse.class);
+
+        //Assert
+        assertEquals(404, result.getStatusCodeValue());
+        assertEquals(404, result.getBody().getCode());
+        assertEquals("Employee not found id=100", result.getBody().getDetail());
+    }
+
+}
